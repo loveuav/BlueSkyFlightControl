f93757c00c34010e2a593f9c29ac7d9efc9071d6
爱无人机 <39085099+loveuav@users.noreply.github.com>
2018-07-24 19:58:26 +0800

    修复USB虚拟串口BUG

--- SRC/DRIVER/drv_usb.c
+++ SRC/DRIVER/drv_usb.c
@@ -4,7 +4,7 @@
                                 技术讨论：bbs.loveuav.com/forum-68-1.html
  * @文件     drv_usb.c
  * @说明     usb转串口驱动
- * @版本  	 V1.1
+ * @版本  	 V1.2
  * @作者     BlueSky
  * @网站     bbs.loveuav.com
  * @日期     2018.07
@@ -14,9 +14,12 @@
 #include "usbd_usr.h"
 #include "usbd_desc.h"
 #include "usb_conf.h" 
+#include "usb_bsp.h"
 #include "usbd_cdc_core.h"
 
 uint32_t connectCheckTime = 0;
+uint8_t  connectFlag = 0;
+uint32_t errorCheckCnt = 0;
 
 USB_OTG_CORE_HANDLE  USB_OTG_dev;
 
@@ -51,12 +54,14 @@ void Usb_Init(void)
 *返 回 值: 无
 **********************************************************************************************************/
 void Usb_Send(uint8_t *dataToSend, uint8_t length)
-{
-    if(GetSysTimeMs() < 3000 || GetSysTimeMs() - connectCheckTime > 2000)
-        return;
+{      
+    if(connectFlag)
+    {
+        DCD_EP_Tx(&USB_OTG_dev, 1, dataToSend, length);
+        DCD_SetEPStatus(&USB_OTG_dev ,1 , USB_OTG_EP_TX_VALID);
+    }
     
-    DCD_EP_Tx(&USB_OTG_dev, 1, dataToSend, length);
-    DCD_SetEPStatus(&USB_OTG_dev ,1 , USB_OTG_EP_TX_VALID);
+    errorCheckCnt = 0;
 }
 
 /**********************************************************************************************************
@@ -78,13 +83,25 @@ void Usb_SetRecvCallback(UsbCallback usbCallback)
 **********************************************************************************************************/
 void Usb_Receive(uint8_t *buf, uint8_t len)
 {
-    connectCheckTime = GetSysTimeMs();
+    connectFlag = 1;
+    errorCheckCnt = 0;
     
     for(u8 i = 0; i<len; i++)
     {
         if(usbCallbackFunc != 0)
             (*usbCallbackFunc)(*(buf+i));
     }
+}
+
+/**********************************************************************************************************
+*函 数 名: Usb_ClearInterrupt
+*功能说明: USB清除中断标志
+*形    参: USB句柄
+*返 回 值: 无
+**********************************************************************************************************/
+void Usb_ClearInterrupt(USB_OTG_CORE_HANDLE *pdev)
+{
+    USB_OTG_WRITE_REG32(&pdev->regs.DREGS->DIEPEMPMSK, 0);
 }
 
 /**********************************************************************************************************
@@ -95,7 +112,14 @@ void Usb_Receive(uint8_t *buf, uint8_t len)
 **********************************************************************************************************/
 void OTG_FS_IRQHandler(void)
 {
-  	USBD_OTG_ISR_Handler(&USB_OTG_dev);
+    if(connectFlag && errorCheckCnt > 200000)
+    {
+        Usb_ClearInterrupt(&USB_OTG_dev);
+        connectFlag = 0;
+    }
+    errorCheckCnt++;
+    
+    USBD_OTG_ISR_Handler(&USB_OTG_dev);
 }  
 
 /**********************************************************************************************************
@@ -108,11 +132,11 @@ void USBD_USR_Init(void)
 {
 } 
 
-void USBD_USR_DeviceReset (uint8_t speed)
+void USBD_USR_DeviceReset(uint8_t speed)
 {
 }
 
-void USBD_USR_DeviceConfigured (void)
+void USBD_USR_DeviceConfigured(void)
 { 
 } 
 
@@ -124,20 +148,12 @@ void USBD_USR_DeviceResumed(void)
 {  
 }
 
-void USBD_USR_DeviceConnected (void)
+void USBD_USR_DeviceConnected(void)
 {
 }
 
-void USBD_USR_DeviceDisconnected (void)
+void USBD_USR_DeviceDisconnected(void)
 {
 }
  
-
-
-
-
-
-
-
-
 
